a3189fe8d2f161c5b09ae04876394b5f24bc38a1
Jason R. Coombs <jaraco@jaraco.com>
2021-01-29 21:22:17 -0500

    ⚫ Fade to black.

--- http_okapi/cherrypy.py
+++ http_okapi/cherrypy.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import
 
 try:
-	import importlib.resources
+    import importlib.resources
 except ImportError:
-	import importlib
-	importlib.resources = importlib.import_module('importlib_resources')
+    import importlib
+
+    importlib.resources = importlib.import_module('importlib_resources')
 
 import cherrypy
 
@@ -12,17 +13,17 @@ import http_okapi
 
 
 class Server:
-	@cherrypy.expose
-	def index(self):
-		"""
-		>>> srv = Server()
-		>>> srv.index()
-		b'...'
-		"""
-		cherrypy.response.headers['Content-Type'] = 'text/html'
-		doc = importlib.resources.read_text(http_okapi, 'okapi.html')
-		return doc.encode('utf-8')
+    @cherrypy.expose
+    def index(self):
+        """
+        >>> srv = Server()
+        >>> srv.index()
+        b'...'
+        """
+        cherrypy.response.headers['Content-Type'] = 'text/html'
+        doc = importlib.resources.read_text(http_okapi, 'okapi.html')
+        return doc.encode('utf-8')
 
-	@cherrypy.expose
-	def okapibg_png(self):
-		return importlib.resources.read_binary(http_okapi, 'okapibg.png')
+    @cherrypy.expose
+    def okapibg_png(self):
+        return importlib.resources.read_binary(http_okapi, 'okapibg.png')
